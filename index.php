fc25a94e8d8c56d1684fbc5f1d1889b230f6f044
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-08-13 19:03:28 +0200

    added word color based on occurence

--- index.php
+++ index.php
@@ -11,63 +11,32 @@
 		<h2>Made with PHP 8 with love <3 </h2>
 	</div>
 
-<?php
-	// Iterate over the file to get occurences of every word
-	$myfile = fopen("bible_copy.txt", "r") or die("Unable to open file!");
-	$dictionary = array();
-	$blacklist = array("the", "and", "in", "of", "to", "a", "it", "", " ", "was", "that", "he", "i", "be", "is");
-	while(!feof($myfile)) {
-		$line = strtolower(fgets($myfile));
-		$line = trim(trim($line), ",.;!?i:");	// Remove undesired characters
-		$words = explode(" ", $line);
-		foreach($words as $word) {
-			if (!in_array($word, $blacklist)) {
-				if (!array_key_exists($word, $dictionary)) {
-    					$dictionary[$word] = 1; 
-				} else {
-					$dictionary[$word] = $dictionary[$word] + 1;
-				}
-			}
-		}
-	}
-	fclose($myfile);
-	arsort($dictionary, SORT_NUMERIC);	// Sort descending
-?>
+	<?php
+	include 'main.php';
+	$file = "bible_copy.txt";
+	$dictionary = count_words($file);
+	?>
+
 	<div class="container">
-	<table class="">
-	  <tr>
-	    <th>Word</th>
-	    <th>Count</th>
-	  </tr>
-<?php
-	// Iterate through each word and create a table of occurences
-	foreach($dictionary as $k => $v) {
-		echo "<tr><th>". $k . "</th><th>" . $v . "</th></tr>";
-	}
-?>
-	</table>
-	
-	<div class="bible">
-<?php
-	// Print every word with a color based on the occurence of the word
-	$myfile = fopen("bible.txt", "r") or die("Unable to open file!");
-	$max = current($dictionary);
-	while(!feof($myfile)) {
-		$line = strtolower(fgets($myfile));
-		$line = trim(trim($line), ",.;!?i:");
-		$words = explode(" ", $line);
-			foreach($words as $word) {
-			if (!in_array($word, $blacklist)) {
-				echo $word . " ";	
-			} else {
-				echo $word . " ";
+		<table class="">
+		  <tr>
+		    <th>Word</th>
+		    <th>Count</th>
+		  </tr>
+			<?php
+			// Iterate through each word and create a table of occurences
+			foreach($dictionary as $k => $v) {
+				echo "<tr><th>". $k . "</th><th>" . $v . "</th></tr>";
 			}
-		}
-		echo "<br>";
-	}
-	fclose($myfile);
-?>
-	</div>
+			?>
+		</table>
+
+
+		<div class="bible">
+		<?php
+			print_colored_words($file, $dictionary);
+		?>
+		</div>
 	</div>
 
 </body>
--- main.php
+++ main.php
@@ -1,4 +1,64 @@
+<?php
+
+$blacklist = array("the", "and", "in", "of", "to", "a", "it", "", " ", "was", "that", "he", "i", "be", "is");
+
 function count_words($file_path)
 {
+	global $blacklist;
+	$myfile = fopen($file_path, "r") or die("Unable to open file!");
+	$dict = array();
 	
+	while(!feof($myfile)) {
+		$line = strtolower(fgets($myfile));
+		$line = trim(trim($line), ",.;!?i:");	// Remove undesired characters
+		$words = explode(" ", $line);
+		foreach($words as $word) {
+			if (!in_array($word, $blacklist)) {
+				if (!array_key_exists($word, $dict)) {
+					$dict[$word] = 1; 
+				} else {
+					$dict[$word] = $dict[$word] + 1;
+				}
+			}
+		}
+	}
+	fclose($myfile);
+	arsort($dict, SORT_NUMERIC);
+	return $dict;
 }
+
+function create_shade($r, $g)
+{
+	$rgb_shade = 'rgb(' . $r . ', ' . $g . ', 0)';
+	return $rgb_shade;
+}
+
+function print_colored_words($filepath, $dict)
+{
+	global $blacklist;
+	$max = current($dict);
+	$red = array(255,0,0);
+	$green = array(0, 255, 0);
+	$myfile = fopen($filepath, "r") or die("Unable to open file!");
+	
+	while(!feof($myfile)) {
+		$line = strtolower(fgets($myfile));
+		$line = trim(trim($line), ",.;!?i:");
+		$words = explode(" ", $line);
+
+			foreach($words as $word) {
+				if (!in_array($word, $blacklist)) {
+					$occurence = $dict[$word];
+					$percentage = ($occurence / $max);
+					$green_factor = round(255 + $percentage * -255);
+					$red_factor = round($percentage * 255);
+					echo '<span style="color:' . create_shade($red_factor, $green_factor) . '">'  . $word . ' ' . '</span>';	
+				} else {
+					echo $word . " ";
+				}
+		}
+		echo "<br>";
+	}
+	fclose($myfile);
+}
+?>
